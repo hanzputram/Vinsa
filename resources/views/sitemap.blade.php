32e4fc3f4c413fd053385debb26531670fb4838d
hanzputram <codmm7083@gmail.com>
2026-02-02 09:52:54 +0700

    update-sitemap-blade

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,30 +1,35 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    {{-- Static Pages --}}
+
+    <!-- Static Pages -->
     <url>
         <loc>{{ url('/') }}</loc>
         <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
+
     <url>
         <loc>{{ url('/about') }}</loc>
         <lastmod>2024-01-01T00:00:00+00:00</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>
+
     <url>
         <loc>{{ url('/product') }}</loc>
-        <lastmod>{{ $products->max('updated_at')?->tz('UTC')->toAtomString() ?? now()->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod>{{ optional($products->max('updated_at'))->tz('UTC')->toAtomString() ?? now()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.9</priority>
     </url>
+
     <url>
         <loc>{{ url('/blog') }}</loc>
-        <lastmod>{{ $blogs->max('updated_at')?->tz('UTC')->toAtomString() ?? now()->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod>{{ optional($blogs->max('updated_at'))->tz('UTC')->toAtomString() ?? now()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.9</priority>
     </url>
+
     <url>
         <loc>{{ url('/contact-us') }}</loc>
         <lastmod>2024-01-01T00:00:00+00:00</lastmod>
@@ -32,7 +37,7 @@
         <priority>0.7</priority>
     </url>
 
-    {{-- Product Pages --}}
+    <!-- Product Pages -->
     @foreach ($products as $product)
         <url>
             <loc>{{ route('product.show', $product->slug) }}</loc>
@@ -42,7 +47,7 @@
         </url>
     @endforeach
 
-    {{-- Blog Pages --}}
+    <!-- Blog Pages -->
     @foreach ($blogs as $blog)
         <url>
             <loc>{{ route('blog.public', $blog->slug) }}</loc>
@@ -51,4 +56,5 @@
             <priority>0.8</priority>
         </url>
     @endforeach
+
 </urlset>
