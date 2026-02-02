a2e7aad6e550e694e046bf377914304dab5dc344
hanzputram <codmm7083@gmail.com>
2026-02-02 10:00:16 +0700

    try-fix-sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,6 +1,12 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://vinsa.fr/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>
     <!-- Static Pages -->
     <url>
         <loc>{{ url('/') }}</loc>
