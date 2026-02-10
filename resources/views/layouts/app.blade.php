f40668370c54fd49dba7d02038b802d51835e025
hanzputram <codmm7083@gmail.com>
2026-02-10 08:37:00 +0700

    fixing-vinsa-color-navbar-admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
                 <button @click="sidebarOpen = !sidebarOpen" class="p-2 rounded-lg hover:bg-slate-100 lg:hidden">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </button>
-                <div class="font-bold text-xl text-indigo-600">Vinsa</div>
+                <div class="font-bold text-xl text-[#066c5f]">Vinsa</div>
                 <div class="w-8"></div><!-- Spacer -->
             </header>
 
